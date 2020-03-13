d2dfdf8bda18eb3a41741cf96996aa46a4db70f7
Brian Brunner <b.brunnerthurman@wsu.edu>
2020-03-13 08:29:05 -0700

    Update to main.cpp
    Fixed values, improved compiling time, and added comments

--- Subsystems/Electronics/Testing/ElectronicLoad/Software/src/main.cpp
+++ Subsystems/Electronics/Testing/ElectronicLoad/Software/src/main.cpp
@@ -1,46 +1,65 @@
 #include "Functions.h"
 
-void init();
-void lcdFunction(int selectedDigit);
+void init(); //Sets current = 0
 
 int main(void)
 {
   int selectedDigit = 0, timesPressed = 0, currentCurrent = 0;
-  Knob niceKnob = Knob(KNOB_CHANNEL_A, KNOB_CHANNEL_B);
-  ST7565 lcd = ST7565(LCD_MOSI, LCD_SCK, LCD_CS1_N, LCD_RST_N, LCD_A0);
+  Knob niceKnob = Knob(KNOB_CHANNEL_A, KNOB_CHANNEL_B); //Initializing the niceKnob Knob class
+  ST7565 lcd = ST7565(LCD_MOSI, LCD_SCK, LCD_CS1_N, LCD_RST_N, LCD_A0); //Initializing the LCD screen
 
-  currentCurrent = getCurrentCurrent();
+  void lcdFunction(int selectedDigit); //Runs the LCD screen
+
+  currentCurrent = getCurrentCurrent(); //Currently set value for electronic current
 
   if (niceKnob.isPressed() == 1)
   {
-    timesPressed++;
+    timesPressed++; // Counts amount of times that the button has been pressed
   }
   else
   {
-    int determineHDValue(int selectedDigit, int timesPressed);
-    void highlightDigit(int selectedDigit, int currentCurrent, ST7565 lcd);
+    int determineHDValue(int selectedDigit, int timesPressed); //Calls digit selection function
+    void highlightDigit(int selectedDigit, int currentCurrent, ST7565 lcd); //Calls digit highlighting function
   }
-  
-
 }
 
+/**
+ * @brief Takes in selectedDigit and timesPressed to determine
+ * which digit is supposed to be highlighted. This is achieved
+ * by verifying if the digit has gone through the whole integer
+ * or not
+ * 
+ * @param selectedDigit 
+ * @param timesPressed 
+ * @return int 
+ */
 int determineHDValue(int selectedDigit, int timesPressed)
 {
-  if (selectedDigit >= 5)
+  if (selectedDigit >= 3)
   {
-    selectedDigit = selectedDigit + timesPressed;
+    selectedDigit = selectedDigit + timesPressed; //Determines current value of selectedDigit
   }
-
-  selectedDigit = selectedDigit + timesPressed;
+  else
+  {
+    selectedDigit = 0; //Resets selectedDigit
+  }
   return selectedDigit;
 }
 
-
+/**
+ * @brief Takes in selectedDigit, currentCurrent, and lcd to change
+ * the selected digit to be a negative image or in this case the 
+ * method used to highlight this digit
+ * 
+ * @param selectedDigit 
+ * @param currentCurrent 
+ * @param lcd 
+ */
 void highlightDigit(int selectedDigit, int currentCurrent, ST7565 lcd){
 int hdValue = 0, xCoordLocation = 0;
 
 xCoordLocation = selectedDigit + 16;
-hdValue = (currentCurrent / pow(10, selectedDigit)); // Probably doesn't reveal the actual digit's value
+hdValue = (currentCurrent % (10 * (int)pow(10, selectedDigit))); // Reveals the value of the digit in the selectedDigit's location
 
 lcd.drawchar(xCoordLocation, 1, hdValue, true);
 }
